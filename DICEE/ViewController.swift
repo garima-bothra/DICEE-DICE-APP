4a84c640c557b2827ef56d56ee2d565dd256e8ba
garima94921 <gaarimabothra@gmail.com>
2019-11-28 16:44:52 +0530

    First commit

---
+++ DICEE/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  DICEE
+//
+//  Created by Garima Bothra on 20/07/19.
+//  Copyright © 2019 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateDiceImages()
+        // Do any additional setup after loading the view.
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages()
+    {
+        
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named:diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named:diceArray[randomDiceIndex2])
+        
+    }
+    
+}
+
